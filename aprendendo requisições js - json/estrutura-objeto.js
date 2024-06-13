f2b6f0d35ea65eaad9f4ae8dd97a942a1dcf4138
victorcavalaro <victorcavalaro298@gmail.com>
2024-06-13 13:32:46 -0300

    estudo

---
+++ aprendendo requisições js - json/estrutura-objeto.js
@@ -0,0 +1,13 @@
+const estudante = {
+nome: 'victor',
+idade: 23,
+cpf: '13012152954',
+turma: 'ads-3b'
+
+}
+
+
+console.log(estudante.nome);
+console.log('o nome do aluno é', estudante.nome );
+console.log('os tres primeiros numeros do cpf é', estudante.cpf.substring(0, 3));
+console.log("as tres primeiras letras do nome é", estudante.nome.substring(0, 3));
